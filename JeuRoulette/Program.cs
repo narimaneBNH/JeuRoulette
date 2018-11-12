bc35272b4eb5e6e7fb5674aac9bc30eb4f00ecbf
Administrateur <Administrateur@UTILISA-NP1IQQA>
2018-11-12 16:56:50 +0100

    Ajoutez des fichiers projet.

---
+++ JeuRoulette/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuRoulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string rejouer = "o";
+            // Random valeur = new Random();
+            //int aleatoire = valeur.Next(0, 36);
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("*** Bienvenue au jeu de la ROULETTE ***");
+            Console.WriteLine("---------------------------------------");
+
+            int stackBanque = 1000;
+            //Console.WriteLine(aleatoire);
+
+            TestInteger joueur = new TestInteger();
+            var stack =  joueur.GetStack(stackBanque);
+
+            
+            while (stackBanque != 0 && stack != 0)
+            {
+                //je joue =^.^=
+                Random valeur = new Random();
+                int aleatoire = valeur.Next(0, 36);
+
+                var number = joueur.GetNumber();
+                var mise =  joueur.GetMise(stack, number);
+                var stack2 = stack - mise;
+
+
+                if (number == aleatoire)
+                {
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Félicitation! Vous avez gagné! ... Votre stack augmente de " + mise * 36);
+                    Console.WriteLine("Votre stack est actuellement = " + (stack + (mise * 36)));
+                    Console.WriteLine("---------------------------------------");
+                }
+                else if (aleatoire == number && number == 0)
+                {
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Félicitation! Vous avez gagné! ... Votre stack augmente de " + mise * 50);
+                    Console.WriteLine("Votre stack est actuellement = " + (stack + (mise * 50)));
+                    Console.WriteLine("---------------------------------------");
+                }
+                else if (aleatoire % 2 == 0 && number %2 ==0)
+                {
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Félicitation! Vous avez deviné que le chiffre mister '"+aleatoire+"' est pair! ... Votre stack ugmente de " + mise * 20);
+                    Console.WriteLine("Votre stack est actuellement = " + (stack + (mise * 20)));
+                    Console.WriteLine("---------------------------------------");
+                }
+                else if (aleatoire % 2 != 0 && number % 2 != 0)
+                {
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Félicitation! Vous avez deviné que le chiffre mister '" + aleatoire + "' est impair! ... Votre stack augmente de " + mise * 20);
+                    Console.WriteLine("Votre stack est actuellement = " + (stack + (mise * 20)));
+                    Console.WriteLine("---------------------------------------");
+                }
+                else
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("PERDU!!! La roulette a choisi le chiffre : "+aleatoire+ " \nIl vous reste : " + (stack = stack - mise) + " dans votre stack! (°v°)");
+                    Console.WriteLine("---------------------------------------");
+                /* if (stack == 0)
+                 {
+                     Console.WriteLine("Merci de passer à la Banque pour pouvoir rejouer! ^_^");
+                     Console.WriteLine("---------------------------------------");
+                 }*/
+
+                Console.ReadKey();
+            }
+
+        }
+
+    }
+}
---
+++ JeuRoulette/TestInteger.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuRoulette
+{
+    class TestInteger
+    {
+
+        public int GetStack(int stackBanque)
+        {
+            var StackChoisi = 0;
+            bool choix = false;
+            while (choix == false)
+            {
+                Console.WriteLine("- Choisir le stack: ");
+                Console.WriteLine("---------------------------------------");
+                string stackSaisi = Console.ReadLine();
+
+                if (int.TryParse(stackSaisi, out StackChoisi))
+                {
+                    choix = true;
+                    if (StackChoisi >= 0 && StackChoisi <= stackBanque)
+                    {
+                        Console.WriteLine("Vous avez " + StackChoisi + " dans le stack =^.^=");
+                        Console.WriteLine("---------------------------------------");
+                        //return StackChoisi;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Erreur! le nombre de saisi doit être entre 0 et 1000 svp =^.^= ");
+                        choix = false;
+                    }
+
+
+                }
+                else
+                {
+                   // choix = false;
+                    Console.WriteLine("Erreur! le nombre de saisi doit être un chiffre entier!:!:!:!");
+
+                }
+                
+            }
+            return StackChoisi;
+
+        }
+
+        //********************************************************************************************************
+
+        public int GetNumber()
+        {
+            int NbrChoisi = 0;
+            bool choix = false;
+            while (choix == false)
+            {
+                Console.WriteLine("- Choisir Un nombre : ");
+                Console.WriteLine("---------------------------------------");
+                string nbrSaisi = Console.ReadLine();
+
+                if (int.TryParse(nbrSaisi, out NbrChoisi))
+                {
+                    choix = true;
+                    if (NbrChoisi >= 0 && NbrChoisi <= 36)
+                    {
+                        Console.WriteLine("Vous avez choisi le numéro " + NbrChoisi + " Bonne chance =^.^=");
+                        return NbrChoisi;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Erreur! le nombre  doit être entre 0 et 36 svp =^.^= ");
+                        choix = false;
+                    }
+
+
+                }
+                else
+                {
+                    choix = false;
+                    Console.WriteLine("Erreur! le nombre de saisi doit être un chiffre entier!:!:!:!");
+
+                }
+            }
+            return NbrChoisi;
+        }
+
+        //*****************************************************************************
+
+        public int GetMise(int stack, int number)
+        {
+            int NbrMise = 0;
+            bool choix1 = false;
+            while (choix1 == false)
+            {
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("- Choisir Une mise pour le chiffre "+number);
+                Console.WriteLine("---------------------------------------");
+                string nbrSaisi = Console.ReadLine();
+
+                if (int.TryParse(nbrSaisi, out NbrMise))
+                {
+                    choix1 = true;
+                    if ( NbrMise <= stack)
+                    {
+                        Console.WriteLine("Vous avez misé sur le numéro "+number+" un stack de " + NbrMise + " Bonne chance =^.^=");
+                        //Console.WriteLine("Dans le stack il vous reste: "+(GetStack() - GetMise()));
+                        
+                        return NbrMise;
+                    }
+
+                    else
+                    {                        
+                        Console.WriteLine(" (°v°) Erreur! la mise doit être inferieure ou égale à "+stack);
+                        choix1 = false;
+                    }
+
+
+                }
+                else
+                {
+                    choix1 = false;
+                    Console.WriteLine("Erreur! le nombre de saisi doit être un chiffre entier!:!:!:!");
+
+                }
+            }
+            return NbrMise;
+        }
+        //***********************************************
+        /*static string Rejouer(int stack2, int number)
+        {
+            int? g;
+            Console.WriteLine("Voulez-vous une nouvelle partie ? o / n ?");
+            string nbrSaisi = Console.ReadLine();
+            if (nbrSaisi == "o")
+            {
+                Console.WriteLine("Vous avez en stack  : "+stack2);
+                
+
+            }
+               
+
+            return;
+        }
+        */
+
+
+    }
+}
